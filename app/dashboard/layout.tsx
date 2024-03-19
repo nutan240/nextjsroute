4b27792f0e6817fac9e1839e2035f5ea3c843175
nutan kumari <nutananju240@gmail.com>
2024-03-19 18:24:08 +0530

    added crud , create , dispaly delete

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,43 +1,71 @@
-"use client"
+"use client";
+import { Box } from "@mui/material";
 import { useRouter } from "next/navigation";
 import React from "react";
+import Image1 from "../img/image.png";
+
 
 interface DashboardLayoutProps {
   children: React.ReactNode;
 }
 
 const DashboardLayout: React.FC<DashboardLayoutProps> = (props) => {
-    const router=useRouter();
+  const router = useRouter();
   return (
-    <div>
+    <div className="w-[100%]">
       <nav
         style={{
-                            background:
-                              "linear-gradient(to left, #ee7724, #d8363a, #dd3675, #b44593)"
-                          }}
-      className=" text-white py-4 px-5 flex justify-between items-center">
+          background:
+            "linear-gradient(to left, #ee7724, #d8363a, #dd3675, #b44593)", 
+          
+        }}
+        className=" text-white py-4 px-5 flex justify-between items-center"
+      >
         <h3 className="text-xl">LOGO</h3>
         <ul className="flex items-center gap-7 font-bold">
-          <li className="cursor-pointer" onClick={()=>router.push('/dashboard/home')}>Home</li>
-          <li className="cursor-pointer" onClick={()=>router.push('/dashboard/about')}>About us</li>
-          <li className="cursor-pointer" onClick={()=>router.push('/dashboard/contact')}>Contact Us</li>
+          <li
+            className="cursor-pointer"
+            onClick={() => router.push("/dashboard/home")}
+          >
+            Home
+          </li>
+          <li
+            className="cursor-pointer"
+            onClick={() => router.push("/dashboard/about")}
+          >
+            About us
+          </li>
+          <li
+            className="cursor-pointer"
+            onClick={() => router.push("/dashboard/contact")}
+          >
+            Contact Us
+          </li>
           <li className="cursor-pointer">Login</li>
         </ul>
       </nav>
       <div className="flex">
-        <div  
-        //  style={{
-        //                     background:
-        //                       "linear-gradient(to top, #ee7724, #d8363a, #dd3675, #b44593)"
-        //                   }}
- className="h-screen w-[14vw] text-blue-900 text-2xl bg-blue-200 font-semibold">
-          <ul className="pt-4 flex flex-col gap-4 px-5">
-            <li className="cursor-pointer" onClick={()=>router.push('/dashboard')}>Dashboard</li>
-           
-            <li className="cursor-pointer" onClick={()=>router.push('/dashboard/profile')}>Profile</li>
+        <div
+         
+          className="h-screen w-[170px] text-blue-900 text-2xl bg-blue-200 font-semibold "
+        >
+          <ul className="pt-4 flex flex-col gap-4 px-5  w-auto ">
+            <li
+              className="cursor-pointer  w-auto "
+              onClick={() => router.push("/dashboard")}
+            >
+              Dashboard
+            </li>
+
+            <li
+              className="cursor-pointer"
+              onClick={() => router.push("/dashboard/profile")}
+            >
+              Profile
+            </li>
           </ul>
         </div>
-        <div className="">{props.children}</div>
+        <Box sx={{backgroundImage:` url( ${Image1} )` , height:'600px' , width:'900px' , margin:'auto'}}>{props.children}</Box>
       </div>
     </div>
   );
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,41 +1,127 @@
-"use client"
-import { useState } from 'react';
+"use client";
+import { Box, Divider, Modal, Typography } from "@mui/material";
+import { useState, useEffect } from "react";
+import Add from "../component/Add";
+import { IoMdAdd } from "react-icons/io";
+import Swal from "sweetalert2";
+import { RiDeleteBin5Fill } from "react-icons/ri";
+import { CgArrowRightR } from "react-icons/cg";
+import { v4 as uuidv4 } from "uuid";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid white",
+  boxShadow: 24,
+  p: 4,
+  borderRadius: "12px",
+};
 
 const DashboardPage = () => {
-    const [email, setEmail] = useState("");
-    const [submittedEmail, setSubmittedEmail] = useState("");
+  const [text, setText] = useState<string>("");
+  const [submittedTodos, setSubmittedTodos] = useState<
+    { id: string; text: string }[]
+  >([]);
+  const [open, setOpen] = useState<boolean>(false);
 
-    const handleSubmit = (event:any) => {
-        event.preventDefault();
-        setSubmittedEmail(email);
-        setEmail('')
-    };
-    
-    const handleChange = (event:any) => {
-        setEmail(event.target.value);
-    };
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem("todos") || "[]");
+    setSubmittedTodos(storedTodos);
+  }, []);
 
-    return (
-        <div className="px-10 py-10">
-            <h1 className="text-blue-500 text-2xl font-sans font-bold">Dashboard page</h1>
-            <form className="px-4 py-6" onSubmit={handleSubmit}>
-                <input
-                    className="bg-gray-200 px-1 py-2 mr-4"
-                    type="email"
-                    name="email"
-                    value={email}
-                    onChange={handleChange}
-                    placeholder="Enter your email"
+  const handleClose = () => setOpen(false);
+  const handleOpen = () => setOpen(true);
+
+  const handleSubmit = (todo: string) => {
+    const newTodo = { id: uuidv4(), text: todo };
+    const updatedTodos = [...submittedTodos, newTodo];
+    setSubmittedTodos(updatedTodos);
+    handleClose();
+    showSuccessNotification(todo);
+    localStorage.setItem("todos", JSON.stringify(updatedTodos));
+  };
+
+  const handleDelete = (id: string) => {
+    const updatedTodos = submittedTodos.filter((todo) => todo.id !== id);
+    setSubmittedTodos(updatedTodos);
+    localStorage.setItem("todos", JSON.stringify(updatedTodos));
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setText(event.target.value);
+  };
+
+  const showSuccessNotification = (todo: string) => {
+    Swal.fire({
+      icon: "success",
+      title: "Todo Added!",
+      text: `You have added "${todo}" to your todo list.`,
+    });
+  };
+
+  return (
+    <>
+      <Modal
+        open={open}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Add onClose={handleClose} onSubmit={handleSubmit} />
+        </Box>
+      </Modal>
+      <div className=" py-5 w-[80%]">
+        <h1 className="text-blue-500 text-2xl font-sans font-bold"></h1>
+        <button
+          className="btn text-black hover:bg-gray-900 hover:text-white w-[700px] bg-red-700 items-center"
+          onClick={handleOpen}
+        >
+          ADD NEW TASK <IoMdAdd style={{ fontSize: "20px" }} />
+        </button>
+
+        {submittedTodos.length > 0 && (
+          <div className="mt-4">
+            <Typography
+              sx={{
+                color: "darkgreen",
+                fontStyle: "italic",
+                fontSize: "25px",
+                textAlign: "center",
+                fontWeight: "bold",
+              }}
+            >
+              Your Entered Task
+            </Typography>
+            {submittedTodos.map((submittedTodo) => (
+              <Box
+                sx={{ mt: 2 }}
+                key={submittedTodo.id}
+                className="flex items-center"
+              >
+                <CgArrowRightR style={{ fontSize: "20px" }} />
+
+                <Typography
+                  sx={{ pl: 3, borderBottom: "0.4px solid gray", width: "750px" }}
+                >
+                  {submittedTodo.text}
+                </Typography>
+                <RiDeleteBin5Fill
+                  style={{ color: "red" , fontSize:'22px'}}
+                  onClick={() => handleDelete(submittedTodo.id)}
+                  className="ml-2 cursor-pointer"
                 />
-                <button className="px-4 bg-orange-900 py-2 text-white" type="submit">Submit</button>
-            </form>
-            {submittedEmail && (
-                <div className="mt-4">
-                    <p className="text-gray-700"> Email : {submittedEmail}</p>
-                </div>
-            )}
-        </div>
-    );
+                <Divider />
+              </Box>
+            ))}
+          </div>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default DashboardPage;
